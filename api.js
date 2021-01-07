2c776585fd03b1f5291ed4e412fa7fbdd6dd26f2
antoinejoly <jolyantoine@zoho.com>
2021-01-07 10:39:55 +0100

    Fix server

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
-const express = require('express'),
-app = express(),
-const { Client } = require('pg'), // import postgre module
-cors = require('cors'),
-bodyParser = require('body-parser');
+const express = require('express')
+app = express()
+const { Client } = require('pg')
+ // import postgre module
+cors = require('cors')
+bodyParser = require('body-parser')
 
 
 // make server object that contain port property and the value for our server.
@@ -11,7 +12,7 @@ var server = {
 };
 
 // setup database
-const db = new Client({
+const client = new Client({
     host: '127.0.0.1',
     user: 'root',
     password: 'MyPassword',
@@ -22,22 +23,16 @@ const db = new Client({
 app.use(cors())
 app.use(bodyParser.json());
 
-db.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + db.threadId);
-});
+client.connect();
 
 // starting the server
 app.listen( server.port , () => console.log(`Server started, listening port: ${server.port}`));
 
-app.get('/:code', (req, res) => {
+app.get('/:code', async (req, res) => {
   const sql = "SELECT * FROM bdd_treefrei WHERE code=$1"
   const result = await client.query({
     text: sql,
     values: [req.params.code] // ici name et description ne sont pas concaténées à notre requête
   })
-  res.json({ status: 200, result})
+  res.status(200).json(result.rows)
 })
